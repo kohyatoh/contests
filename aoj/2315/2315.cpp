3326bbf2fb6b5620b51b77598885a5c929b77eb2
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2011-09-22 04:26:50 +0900

    aoj 2315

---
+++ aoj/2315/2315.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+#define rep(i, n) for(int i=0; i<(int)(n); i++)
+#define Z (128)
+
+void mul(double (*a)[Z], const double (*b)[Z]) {
+    static double t[Z][Z];
+    memset(t, 0, sizeof(t));
+    rep(k, Z) rep(i, Z) rep(j, Z) t[i][j] += a[i][k]*b[k][j];
+    memcpy(a, t, sizeof(t));
+}
+
+void pow(double (*a)[Z], int x) {
+    static double p[Z][Z];
+    memcpy(p, a, sizeof(p));
+    rep(i, Z) rep(j, Z) a[i][j] = i==j ? 1.0 : 0.0;
+    rep(b, 32) {
+        if(x&(1<<b)) mul(a, p);
+        mul(p, p);
+    }
+}
+
+void eqsolve(int n, double (*a)[Z]) {
+    rep(k, n) {
+        double mx = -1;
+        int ix = -1;
+        for(int i=k; i<n; i++) if(mx<fabs(a[i][k])) mx=fabs(a[i][k]), ix=i;
+        if(k!=ix) rep(i, n+1) swap(a[k][i], a[ix][i]);
+        const double p = a[k][k];
+        for(int i=k; i<=n; i++) a[k][i] /= p;
+        rep(i, n) if(i!=k) {
+            const double d = a[i][k];
+            for(int j=k; j<=n; j++) a[i][j] -= d*a[k][j];
+        }
+    }
+}
+
+int S, N, K;
+double p[Z][Z], eqs[Z][Z], mat[Z][Z];
+
+int main() {
+    scanf("%d%d%d", &S, &N, &K);
+    S = abs(S);
+    if(N==1) {
+        printf("%.9f\n", S%K==0 ? (double)(S/K) : 0.0);
+        return 0;
+    }
+    p[0][0] = 1.0;
+    const int M = N*K+1;
+    rep(i, K) rep(j, M) rep(k, N) p[i+1][j+k+1] += p[i][j]/N;
+    eqs[0][0] = 1;
+    for(int k=1; k<M; k++) {
+        eqs[k][k] = 1;
+        rep(j, M) eqs[k][abs(k-j)] -= p[K][j];
+        eqs[k][M] = -1;
+    }
+    eqsolve(M, eqs);
+    if(S<M) printf("%.9f\n", fabs(eqs[S][M]));
+    else {
+        rep(i, M) mat[0][i] = p[K][i+1];
+        mat[0][M] = 1;
+        rep(i, M-1) mat[i+1][i] = 1;
+        mat[M][M] = 1;
+        pow(mat, S-M+1);
+        double ans = 0;
+        rep(i, M) ans += mat[0][i]*fabs(eqs[M-1-i][M]);
+        ans += mat[0][M];
+        printf("%.9f\n", ans);
+    }
+    return 0;
+}
