e3f73feaa2dd49852bc62a60f6f5cc5d3dd324d1
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2011-09-29 04:49:55 +0900

    use bitset

--- aoj/1308/1308.cpp
+++ aoj/1308/1308.cpp
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <bitset>
 #include <algorithm>
 using namespace std;
 #define rep(i, n) for(int i=0; i<(int)(n); i++)
 
-int M, N, D, S[32][32], a[768][768];
+int M, N, D, S[32][32];
+bitset<640> bits[640];
 
 bool solve(int n) {
     int h = 0;
     rep(k, n) {
         int ix = -1;
-        for(int i=h; i<n; i++) if(a[i][k]) { ix=i; break; }
+        for(int i=h; i<n; i++) if(bits[i][k]) { ix=i; break; }
         if(ix==-1) continue;
-        if(h!=ix) rep(i, n+1) swap(a[h][i], a[ix][i]);
-        rep(i, n) if(i!=h) if(a[i][k]) {
-            for(int j=k; j<=n; j++) a[i][j] ^= a[h][j];
-        }
+        if(h!=ix) swap(bits[h], bits[ix]);
+        rep(i, n) if(i!=h) if(bits[i][k]) bits[i] ^= bits[h];
         h++;
     }
-    for(int i=h; i<n; i++) if(a[i][n]==1) return false;
+    for(int i=h; i<n; i++) if(bits[i][n]) return false;
     return true;
 }
 
@@ -28,13 +28,13 @@ int main() {
         scanf("%d%d%d", &M, &N, &D);
         if(M==0 && N==0 && D==0) return 0;
         rep(i, N) rep(j, M) scanf("%d", S[i]+j);
-        memset(a, 0, sizeof(a));
         const int n = N*M;
         rep(i, N) rep(j, M) {
             const int ix = i*M+j;
-            a[ix][ix] = 1;
-            a[ix][n] = S[i][j];
-            rep(x, N) rep(y, M) if(abs(i-x)+abs(j-y)==D) a[ix][x*M+y] = 1;
+            bits[ix].reset();
+            bits[ix].set(ix);
+            if(S[i][j]) bits[ix].set(n);
+            rep(x, N) rep(y, M) if(abs(i-x)+abs(j-y)==D) bits[ix].set(x*M+y);
         }
         printf("%d\n", (int)solve(n));
     }
