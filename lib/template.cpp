1c06076b544c74d0e16100c558dbb50781714efa
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2012-03-13 08:27:32 +0900

    add utilities

---
+++ lib/template.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <utility>
+#include <algorithm>
+using namespace std;
+#ifdef DEBUGRUN
+#define LOG(a) (cerr<<__LINE__<<": "#a" = "<<(a)<<endl)
+#else
+#define LOG(...) ((void)0)
+#endif
+#define rep(i, n) for(int i=0; i<(int)(n); i++)
+#define mp make_pair
+#define foreach(it, c) for(__typeof((c).begin()) it=(c).begin(); it!=(c).end(); it++)
+typedef long long Int;
+#define INF (MY_INFINITY)
+#define MOD (YOUR_MODULUS)
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    LOG(a);
+    LOG(b);
+    printf("%d\n", a+b);
+    return 0;
+}
---
+++ lib/vimrc
@@ -0,0 +1,4 @@
+se ts=4 sw=4 et cin hls is ml
+syntax on
+map #5 :w:!xxrun %
+map #6 :w:!zzrun %
