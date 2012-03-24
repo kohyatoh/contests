d4ffda73f083bc6a068677fe5b4efa01cdc6d7f5
Kohsuke Yatoh <kohyatoh@yahoo.co.jp>
2012-03-25 04:34:56 +0900

    add atoi and BangPatterns

--- lib/template.hs
+++ lib/template.hs
@@ -1,16 +1,30 @@
+{-# OPTIONS_GHC -O3 -fno-spec-constr-count #-}
+{-# LANGUAGE BangPatterns #-}
 import Control.Monad
+import Control.Applicative
 import Text.Printf
+import Data.List
+import Debug.Trace
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Char as C
+
+atoi :: B.ByteString -> Int
+atoi s | B.head s == '-' = - (atoi' $ B.tail s)
+       | otherwise = atoi' s
+
+atoi' :: B.ByteString -> Int
+atoi' = B.foldl' (\a c -> a*10 + C.ord c - C.ord '0') 0
 
 solve :: Int -> Int
 solve n = n
 
 input :: IO Int
 input = do
-    n <- readLn
+    n <- atoi . head . B.words <$> B.getLine
     return n
 
 main = do
-    n <- readLn :: IO Int
+    n <- atoi . head . B.words <$> B.getLine
     forM_ [1..n] $ \i -> do
         n <- input
         let ans = solve n
